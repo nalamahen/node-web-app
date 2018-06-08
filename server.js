86e15ce42d77dfc6707aad1bbd490b2e5c49c61c
Nala Mahen <mail2mahen@yahoo.co.uk>
2018-06-08 11:59:16 +0100

    Remove portfolio page and add projects page.

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.get('/about', (req, res) => {
    });
 });
 
-app.get('/portfolio', (req, res) => {
-   res.render('portfolio.hbs', {
-       pageTitle: 'Portfolio Page',      
+app.get('/projects', (req, res) => {
+   res.render('projects.hbs', {
+       pageTitle: 'Projects',      
    });
 })
 
